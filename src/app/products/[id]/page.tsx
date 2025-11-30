9420bfcb9f84b7a8562c7ecadc3aba2f49cad0af
Michael <fatlycka@gmail.com>
2025-11-30 17:00:41 +0300

    fix: fix build errors
    fix: fix dynamic route issue
    
    fix: fix build error
    
    fix: fix baseline browsewr mapping error
    
    fix: hope to fix build issue
    
    fix: fix again
    
    fix: fix build issue x2
    
    fix: fix x3

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,33 +1,30 @@
-'use client';
 import RedirectButton from '@/ui/redirect-button/redirect-button';
 import Details from '@/components/details/details';
-import { useProductStore } from '@/stores/product-store';
 import { cn } from '@/utils/cn';
-import { usePathname } from 'next/navigation';
+import { getAllProducts } from '@/api/getData';
+import { URLS } from '@/utils/urls';
 
-export default function Page() {
-  const items = useProductStore((state) => state.items);
+export const dynamicParams = false;
 
-  const pathname = usePathname();
-  const result = pathname.replace(/^\/[a-z]+\//, '');
-  const currentItem = items.products.find((item) => item.id === +result);
+type Slugs = {
+  id: string;
+};
+
+type PageProps = {
+  params: Slugs;
+};
+
+export async function generateStaticParams(): Promise<Slugs[]> {
+  const data = await getAllProducts(URLS.getAll);
+  return data.products.map((item) => ({ id: item.id.toString() }));
+}
+
+export default function Page({ params }: PageProps) {
+  const slug = params.id;
 
   return (
     <div className={cn('flex w-full flex-col items-center justify-center p-5 max-sm:p-1')}>
-      {currentItem ? (
-        <Details currentItem={currentItem} />
-      ) : (
-        <h2 className={cn('m-5 flex flex-col gap-5')}>
-          <span className={cn('text-xl')}>
-            {
-              "Ooops! Since we can't upload created Cards to API and then get em back to our app. I decided to not fetch single card in this particular task xD."
-            }
-          </span>
-          <span className={cn('text-text-card text-center text-lg')}>
-            {'So please go back to Products page and click on the right card ;O'}
-          </span>
-        </h2>
-      )}
+      {<Details slug={slug} />}
       <div className={cn('flex w-full justify-center p-5')}>
         <RedirectButton href="/" title="Go to Main" width={'400px'} />
       </div>
--- src/components/settings/settings.tsx
+++ src/components/settings/settings.tsx
@@ -1,6 +1,10 @@
+'use client';
+import { Suspense } from 'react';
+
 import { cn } from '@/utils/cn';
 import FilterCards from './filter-cards';
 import SearchCards from './search-cards';
+import Loader from '@/ui/loader/loader';
 
 export default function Settings() {
   return (
@@ -9,8 +13,10 @@ export default function Settings() {
         'flex w-full max-w-[1000px] flex-row flex-wrap justify-center gap-5 max-md:flex-col',
       )}
     >
-      <SearchCards />
-      <FilterCards />
+      <Suspense fallback={<Loader />}>
+        <SearchCards />
+        <FilterCards />
+      </Suspense>
     </div>
   );
 }
