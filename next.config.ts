3fc9a0853826970186c2e7f325c3d964e00e0b98
Michael <fatlycka@gmail.com>
2025-11-30 05:29:03 +0300

    refactor: prepare for deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // output: 'export',
-  // basePath: '/testovoe-react-app',
-  // assetPrefix: '/testovoe-react-app',
+  output: 'export',
+  basePath: '/testovoe-react-app',
+  assetPrefix: '/testovoe-react-app',
   images: {
     remotePatterns: [new URL('https://cdn.dummyjson.com/product-images/**')],
   },
