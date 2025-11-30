debb838822a522765d8cddf55f112baa8a722e13
Michael <fatlycka@gmail.com>
2025-11-30 18:07:28 +0300

    fix: fix prefix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/testovoe-react-app',
-  assetPrefix: '/testovoe-react-app',
+  assetPrefix: '/testovoe-react-app/',
   images: {
     remotePatterns: [new URL('https://cdn.dummyjson.com/product-images/**')],
   },
